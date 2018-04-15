b0f312e938dbd705636540073a4ab447c168fe9d
tony-zhou <tony.3.4.1900@gmail.com>
2018-04-16 00:39:39 +0800

    initial week2

---
+++ src/week2/Permutation.java
@@ -0,0 +1,21 @@
+package week2;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by tony on 4/16/18.
+ */
+public class Permutation {
+
+    public static void main(String[] args) {
+        int len = Integer.parseInt(args[0]);
+        RandomizedQueue<String> randomizedQueue = new RandomizedQueue<>();
+        for (int i = 0; i < len; i++) {
+            randomizedQueue.enqueue(StdIn.readString());
+        }
+        for (int j = 0; j < len; j++) {
+            StdOut.println(randomizedQueue.dequeue());
+        }
+    }
+}
--- src/week2/RandomizedQueue.java
+++ src/week2/RandomizedQueue.java
@@ -1,13 +1,33 @@
 package week2;
 
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
-    private int size;
+    private int size = 0;
+    private int index = 0;
+    private Item[] queue;
 
+    @SuppressWarnings("unchecked")
     public RandomizedQueue() {
+        queue = (Item[]) new Object[1];
+    }
+
+    @SuppressWarnings("unchecked")
+    private void resize(int len) {
+        Item[] tmpQueue = (Item[]) new Object[len];
+        int index = 0;
+        for (int i = 0; i < queue.length; i++) {
+            if (queue[i] != null) {
+                tmpQueue[index] = queue[i];
+                index++;
+            }
+        }
+        queue = tmpQueue;
     }
 
     public boolean isEmpty() {
@@ -21,29 +41,65 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     public void enqueue(Item item) {
         if (item == null)
             throw new IllegalArgumentException();
+        if (queue.length <= index) {
+            resize(2 * size);
+        }
+        queue[index++] = item;
+        size++;
     }
 
     public Item dequeue() {
         if (isEmpty())
-            throw  new NoSuchElementException();
+            throw new NoSuchElementException();
+        Item item = null;
+        while (item == null) {
+            int position = StdRandom.uniform(0, index);
+            item = queue[position];
+            queue[position] = null;
+        }
+        size--;
+        if (size < queue.length / 4) {
+            resize(2 * size);
+        }
+        return item;
     }
 
     public Item sample() {
         if (isEmpty())
             throw new NoSuchElementException();
+        Item item;
+        int position;
+        do {
+            position = StdRandom.uniform(0, index);
+            item = queue[position];
+        } while (item == null);
+        return item;
     }
 
     @Override
     public Iterator<Item> iterator() {
         return new Iterator<Item>() {
+
+            private Item[] tmp = queue.clone();
+            private int count = size;
+
             @Override
             public boolean hasNext() {
-                return false;
+                return count != 0;
             }
 
             @Override
             public Item next() {
-                return null;
+                if (!hasNext())
+                    throw new NoSuchElementException();
+                Item item = null;
+                while (item == null) {
+                    int position = StdRandom.uniform(0, index);
+                    item = tmp[position];
+                    tmp[position] = null;
+                }
+                count--;
+                return item;
             }
 
             @Override
@@ -53,6 +109,19 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         };
     }
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        RandomizedQueue<Character> randomizedQueue = new RandomizedQueue<>();
+        randomizedQueue.enqueue('A');
+        randomizedQueue.enqueue('B');
+        randomizedQueue.enqueue('C');
+//        StdOut.println(randomizedQueue.sample());
+//        StdOut.println(randomizedQueue.dequeue());
+//        StdOut.println(randomizedQueue.sample());
+//        StdOut.println(randomizedQueue.sample());
+        for (char item: randomizedQueue)
+            StdOut.println(item);
+        for (char item: randomizedQueue)
+            StdOut.println(item);
+    }
 
 }
